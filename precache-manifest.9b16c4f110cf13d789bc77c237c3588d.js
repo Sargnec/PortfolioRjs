0f7e04c78a5cc29cadb8f0b29624040380d12aaf
Sargnec <49839699+Sargnec@users.noreply.github.com>
2019-08-05 15:42:26 +0300

    Updates

---
+++ precache-manifest.9b16c4f110cf13d789bc77c237c3588d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95a0f63506227ec76ed6bbff8ffebc0",
+    "url": "/PortfolioRjs/index.html"
+  },
+  {
+    "revision": "3d83992246989a97bdb1",
+    "url": "/PortfolioRjs/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "61f67dc01ff8369a42f5",
+    "url": "/PortfolioRjs/static/css/main.dfe02fe3.chunk.css"
+  },
+  {
+    "revision": "3d83992246989a97bdb1",
+    "url": "/PortfolioRjs/static/js/2.a437ad5d.chunk.js"
+  },
+  {
+    "revision": "61f67dc01ff8369a42f5",
+    "url": "/PortfolioRjs/static/js/main.e8ada4d1.chunk.js"
+  },
+  {
+    "revision": "7cd9b36d6887168b52dd",
+    "url": "/PortfolioRjs/static/js/runtime~main.753fa691.js"
+  }
+]);
